6ec04aab72747db4f38dcc48a15c67080212a2b2
wyebani <tomczykmarek33@gmail.com>
2018-12-02 18:00:07 +0100

    Add new functionality in AdminPanel

--- php/class/AdminPanel.Class.php
+++ php/class/AdminPanel.Class.php
@@ -10,8 +10,83 @@ require 'Crud.Class.php';
 
 class AdminPanel extends Crud {
     
+/*******************************************************************************
+ * @brief                                                                      *
+ *       Method gets all Users from database.                                  *
+ * @params:                                                                    *
+ *      - none                                                                 *
+ * @returns:                                                                   *
+ *      - array with users                                                     *
+ *      - false when fail                                                      *
+ ******************************************************************************/
     
+    public function getAllUsers() {
+        return $this->getAll('user');
+    }
+
+/*******************************************************************************
+ * @brief                                                                      *
+ *       Method adds new user into database.                                   *
+ * @params:                                                                    *
+ *      -$sUsername - user login                                               *
+ * @returns:                                                                   *
+ *      - user ID when success                                                 *
+ *      - false when fail                                                      *
+ ******************************************************************************/
     
+    public function addNewUser($sUsername) {
+        $iId = $this->addOne('user',
+                        array('username' => $sUsername,
+                        'password' => hash('sha512', 'haslo'),
+                        'permissions' => 0,
+                        'active' => 1));
+        
+        if($iId != null) {
+            $this->addOne('user_details',
+                        array('id' => $iId));
+        } else {
+            return false;
+        }
+        
+        return $iId;
+    }
+    
+/*******************************************************************************
+ * @brief                                                                      *
+ *       Method deletes user from database.                                    *
+ * @params:                                                                    *
+ *      -$iId - user ID                                                        *
+ * @returns:                                                                   *
+ *      - true when success                                                    *
+ *      - false when fail                                                      *
+ ******************************************************************************/    
+    
+    public function deleteUser($iId) {
+        $bResult = $this->delete('user', $iId);
+        if($bResult) {
+            $bResult = $this->delete('user_details', $iId);
+        } else {
+            return false;
+        }
+        
+        return $bResult;
+    }
+    
+/*******************************************************************************
+ * @brief                                                                      *
+ *       Method activate user.                                                 *
+ * @params:                                                                    *
+ *      -$iId - user ID                                                        *
+ * @returns:                                                                   *
+ *      - true when success                                                    *
+ *      - false when fail                                                      *
+ ******************************************************************************/     
+    
+    public function activateUser($iId) {
+        return $this->update('user',
+                    array('active' => 1),
+                    array('id' => $iId));
+    }
     
 }
 
