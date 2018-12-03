69f387e2abb79be5a2104f21beb61992e9a39d23
wyebani <tomczykmarek33@gmail.com>
2018-12-03 16:44:43 +0100

    Add services into Admin Panel Class

--- php/class/AdminPanel.Class.php
+++ php/class/AdminPanel.Class.php
@@ -1,139 +1,31 @@
 <?php
 
-require 'Crud.Class.php';
+require 'service/UserService.Class.php';
+require 'service/ArticleService.Class.php';
+require 'service/LeagueService.Class.php';
+// require 'service/TeamService.Class.php';
+// require 'service/MatchService.Class.php';
 
 /*******************************************************************************
  * @brief Admin panel class                                                    *
- * @author Marek                                                               *
+ * @author Marek, Paweł                                                        *
  * @date 01.12.2018                                                            *
  ******************************************************************************/
 
-class AdminPanel extends Crud {
+class AdminPanel {
+    var $oUserService;
+    var $oArticleService;
+    var $oLeagueService;
+//  var $oTeamService;
+//  var $oMatchService;    
     
-/*******************************************************************************
- * @brief                                                                      *
- *       Method gets all Users from database.                                  *
- * @params:                                                                    *
- *      - none                                                                 *
- * @returns:                                                                   *
- *      - array with users                                                     *
- *      - false when fail                                                      *
- ******************************************************************************/
-    
-    public function getAllUsers() {
-        return $this->getAll('user');
+    public function __construct() {
+        $this->oUserService = new UserService();
+        $this->oArticleService = new ArticleService();
+        $this->oLeagueService = new LeagueService();
+//      $this->oTeamService = new TeamService();
+//      $this->oMatchService = new MatchService();  
     }
-
-/*******************************************************************************
- * @brief                                                                      *
- *       Method adds new user into database.                                   *
- * @params:                                                                    *
- *      -$sUsername - user login                                               *
- * @returns:                                                                   *
- *      - user ID when success                                                 *
- *      - false when fail                                                      *
- ******************************************************************************/
-    
-    public function addNewUser($sUsername) {
-        if(empty($sUsername)) {
-            return false;
-        }
-        
-        $iId = $this->addOne('user',
-                        array('username' => $sUsername,
-                        'password' => hash('sha512', 'haslo'),
-                        'permissions' => 0,
-                        'active' => 1));
-        
-        if($iId != null) {
-            $this->addOne('user_details',
-                        array('id' => $iId));
-        } else {
-            return false;
-        }
-        
-        return $iId;
-    }
-    
-/*******************************************************************************
- * @brief                                                                      *
- *       Method deletes user from database.                                    *
- * @params:                                                                    *
- *      -$iId - user ID                                                        *
- * @returns:                                                                   *
- *      - true when success                                                    *
- *      - false when fail                                                      *
- ******************************************************************************/    
-    
-    public function deleteUser($iId) {
-        $bResult = false;
-        
-        if($iId != null) {
-            $bResult = $this->delete('user', $iId);
-            if($bResult) {
-                $bResult = $this->delete('user_details', $iId);
-            } else {
-                return false;
-            }
-        }     
-        return $bResult;
-    }
-    
-/*******************************************************************************
- * @brief                                                                      *
- *       Method activate user.                                                 *
- * @params:                                                                    *
- *      -$iId - user ID                                                        *
- * @returns:                                                                   *
- *      - true when success                                                    *
- *      - false when fail                                                      *
- ******************************************************************************/     
-    
-    public function activateUser($iId) {
-        if($iId != null) {
-            return $this->update('user',
-                    array('active' => 1),
-                    array('id' => $iId));
-        }
-        return false;
-    }
-    
-/*******************************************************************************
- * @brief                                                                      *
- *       Method for update user.                                               *
- * @params:                                                                    *
- *      -$aUserData - array with user fields                                   *
- * @returns:                                                                   *
- *      - true when success                                                    *
- *      - false when fail                                                      *
- ******************************************************************************/
-
-    public function updateUser($aUserData) {
-        if(empty($aUserData)) {
-            return false;
-        }
-        
-        $bResult = false;
-        
-        $bResult = $this->update('user', 
-                array('username' => $aUserData['username'],
-                            'password' => hash('sha512', $aUserData['password']),
-                            'permissions' => $aUserData['permissions'],
-                            'active' => $aUserData['active']),
-                array('id' => $aUserData['id']));
-        
-        if($bResult) {
-             $this->update('user_details',
-                    array('name' => $aUserData['name'],
-                            'surname' => $aUserData['surname'],
-                            'date_of_birth' => $aUserData['date_of_birth'],
-                            'email' => $aUserData['email']),
-                    array('id' => $aUserData['id']));
-        }
-        
-        return $bResult;
-    }
-    
 }
 
 /*******************************************************************************
