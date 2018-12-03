d54ebc7020c2e67fd1b765eb907037df86757731
pawels313 <45108672+pawels313@users.noreply.github.com>
2018-12-03 21:21:00 +0100

    Add files via upload

---
+++ php/class/service/TeamService.Class.php
@@ -0,0 +1,132 @@
+<?php
+
+require '../class/Crud.Class.php';
+
+/*******************************************************************************
+ * @brief Service for entity Team																						*
+ * @author Pawel																											*
+ * @date 03.12.2018																										*
+ ******************************************************************************/
+
+class TeamService extends Crud {
+    
+/*******************************************************************************
+ * @brief																														*
+ *       Method gets all Teams from database.																		*	
+ * @params:																													*
+ *      - none 																													*
+ * @returns:																													*
+ *      - array with users																									*
+ *      - false when fail																										*
+ ******************************************************************************/
+    
+    public function getAllTeams() {
+        return $this->getAll('team');
+    } 
+    
+/*******************************************************************************
+ * @brief                                                                      													*
+ *       Method gets one Team from database.																		*		
+ * @params:																													*
+ *      - $iId - team ID																										*
+ * @returns:                                                            												       	*
+ *      - array with teams																									*
+ *      - false when fail																										*
+ ******************************************************************************/
+    
+    public function getTeam($iId) {
+        return $this->getAll('team', $iId);
+    }     
+    
+/*******************************************************************************
+ * @brief                                                                      													*
+ *       Method adds new team into database.																		*
+ * @params:                                                                    												*
+ *      -$sTeamName - team name                                         											*                                             
+ * @returns:       	                                                           												*
+ *      - team ID when success																							*
+ *      - false when fail                                                      												*
+ ******************************************************************************/    
+    
+    public function addNewTeam($sTeamName) {
+        if(empty($sTeamName)) {
+            return false;
+        }
+        
+        $iId = $this->addOne('team',
+                        array('name' => $sTeamName));
+        
+        if($iId != null) {
+            $this->addOne('team_details',
+                        array('id' => $iId));
+        } else {
+            return false;
+        }
+        
+        return $iId;
+    }
+    
+/*******************************************************************************
+ * @brief																														*
+ *       Method deletes team from database.																		*
+ * @params:																													*
+ *      -$iId - team ID																										*
+ * @returns:																													*
+ *      - true when success																								*
+ *      - false when fail																										*
+ ******************************************************************************/    
+    
+    public function deleteTeam($iId) {
+        $bResult = false;
+        
+        if($iId != null) {
+            $bResult = $this->delete('team', $iId);
+            if($bResult) {
+                $bResult = $this->delete('team_details', $iId);
+            } else {
+                return false;
+            }
+        }     
+        return $bResult;
+    }
+    
+/*******************************************************************************
+ * @brief                                                                      													*
+ *       Method for update team.                                           												*
+ * @params:                                                                    												*
+ *      -$aTeamData - array with team fields                               											*
+ * @returns:                                                                   												*
+ *      - true when success                                                    											*
+ *      - false when fail                                                      												*
+ ******************************************************************************/
+
+    public function updateTeam($aTeamData) {
+        if(empty($aTeamData)) {
+            return false;
+        }
+        
+        $bResult = false;
+        
+        $bResult = $this->update('team', 
+                array('name' => $aTeamData['name']),                            
+                array('id' => $aTeamData['id']));
+        
+        if($bResult) {
+             $this->update('team_details',
+                    array('founder' => $aTeamData['founder'],
+                            'date_of_found' => $aTeamData['date_of_found'],
+							'ground' => $aTeamData['ground'],
+							'president' => $aTeamData['president'],
+							'head_couch' => $aTeamData['head_couch'],
+							'league_id' => $aTeamData['league_id'],
+							'website' => $aTeamData['website']),
+                    array('id' => $aTeamData['id']));
+        }
+        
+        return $bResult;
+    }    
+}
+
+/*******************************************************************************
+ *                              END OF FILE                                    												*
+ ******************************************************************************/
