626fb6e7b9c970d979576a5ab4562515c631c0a0
wyebani <tomczykmarek33@gmail.com>
2018-12-19 22:13:29 +0100

    Add AJAX JQuery dynamic pages loading

--- javascript/Click_admin.js
+++ javascript/Click_admin.js
@@ -1,52 +1,76 @@
 window.onload = function() {
-  var number=1;//user_menu
-  var temporary=1;
-  var menu = ["Users", "league", "teams","match", "articles"];
+  var number=0;
+  var temporary=0;
+  var menu = ["user", "league", "teams","match", "articles"];
   
   function Click_panel(){
+    for(var i=0; i < menu.length; i++){
+        if(i === temporary)
+            document.getElementById("" + menu[i]).style.display = "none";
+    }
     
-  
-          for(var i=1;i<=menu.length;i++){
-          
-          if(i===temporary)  document.getElementById(""+menu[i-1]).style.display = "none";
-      }
-          document.getElementById(""+menu[number-1]).style.display = "block";
-          temporary=number;
-          
-     
-      
-      
+    document.getElementById("" + menu[number]).style.display = "block";
+    temporary=number;     
   }
   
-  function _users(){
-      number=1;
-       Click_panel();
-       
+  function _user() {
+    number=0;
+    
+    $('#usersList').html('');
+    $.ajax({
+        method: "GET",
+	url: "../functions/admin_panel/user/getAllUsers.php"
+    }).done(function (msg) {
+        $('#usersList').html(msg);
+    });
+    
+    Click_panel();
   }
+  
   function _league(){
-      number=2;
-       Click_panel();
+      number=1;
+      
+      $.ajax({
+          method: "GET",
+          url: "../functions/admin_panel/league/getAllLeagues.php"
+      }).done(function (msg) {
+         $('#leaguesList').html(msg); 
+      });
+      
+      Click_panel();
   }
    function _teams(){
-      number=3;
+      number=2;
+      
+      $.ajax({
+          method: "GET",
+          url: "../functions/admin_panel/league/getAllLeagues.php"
+      }).done(function (msg) {
+         $('#leaguesList_2').html(msg); 
+      });
+      
+      $.ajax({
+          method: "GET",
+          url: "../functions/admin_panel/team/getAllTeams.php"
+      }).done(function (msg) {
+         $('#teamsList').html(msg); 
+      });
+      
       Click_panel();
   }
     function _match(){
-      number=4;
+      number=3;
       Click_panel();
   }
    function _articles(){
-      number=5;
+      number=4;
       Click_panel();
   }
  
   
-  document.getElementById("_Users").onclick = function() {_users()};
-  document.getElementById("_league").onclick = function() {_league()};
-  document.getElementById("_teams").onclick = function() {_teams()};
-  document.getElementById("_match").onclick = function() {_match()};
-  document.getElementById("_articles").onclick = function() {_articles()};
-  
-  
-  
+  document.getElementById("_users").onclick = function() { _user() };
+  document.getElementById("_league").onclick = function() { _league() };
+  document.getElementById("_teams").onclick = function() { _teams() };
+  document.getElementById("_match").onclick = function() { _match() };
+  document.getElementById("_articles").onclick = function() { _articles() }; 
 };
