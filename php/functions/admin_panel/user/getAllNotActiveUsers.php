c54fcfa2c0ff667516bf0acd9dc3181f1b2c9d5c
wyebani <tomczykmarek33@gmail.com>
2018-12-20 20:51:09 +0100

    Add new function to get all not active users from DB

---
+++ php/functions/admin_panel/user/getAllNotActiveUsers.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__.'/../../../class/service/UserService.Class.php';
+
+$oUserService = new UserService();
+$aUsers = $oUserService->getAllNotActiveUsers();
+
+if($aUsers) {
+    echo '<input list="not_active_users" name="not_active_user" class="inputclass"/>';
+    echo '<datalist id="not_active_users_list">';
+    foreach ($aUsers as $key => $value) {
+        echo '<option value="'.$value['id'].'">'.$value['username'].'</option>';
+    }
+    echo '</datalist>';
+} else {
+    echo '<strong style="color: red; font-size: 14px">Brak nieaktywnych użytkowników w bazie danych!</strong>';
+}
+
