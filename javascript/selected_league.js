a419738c098f408e6e2cb5ad82fa7396c76c0dfa
wyebani <tomczykmarek33@gmail.com>
2019-01-13 17:39:33 +0100

    Refactor code

--- javascript/selected_league.js
+++ javascript/selected_league.js
@@ -1,138 +1,121 @@
 $("document").ready(function () {
     var iterator = 0;
-    var arrayarticle = ["First", "Second", "Third"];
-    var numberofarticles = new Array();
-     start();
-     gettable();
+    var arrayArticle = ["First", "Second", "Third"];
+    var numOfArticles = new Array();
     
-   //flags
-        document.getElementById("poland").onclick = function (){openwindow("poland","Ekstraklasa");}
-        document.getElementById("germany").onclick = function (){openwindow("germany","Bundesliga");}
-        document.getElementById("spain").onclick = function (){openwindow("spain","LaLiga");}
-        document.getElementById("portugal").onclick = function (){openwindow("portugal","Portugalska");}
-        document.getElementById("italy").onclick = function (){openwindow("italy","Seria_A");}
-        document.getElementById("brazil").onclick = function (){openwindow("brazil","Brazylijska");}
-        document.getElementById("england").onclick = function (){openwindow("england","Angielska");}
-        document.getElementById("france").onclick = function (){openwindow("france","Ligue1");}
-        
-    function openwindow(country,leaguename){
-        //alert('lala');
-        //myWindow = window.open("selected_league.php?league="+leaguename);   // Opens a new window
-          document.getElementById('a'+country).href="selected_league.php?league="+leaguename;
-            
+    start();
+    getTable();
+
+    //flags
+    document.getElementById("poland").onclick = function () {
+        openWindow("poland", "Ekstraklasa");
     }
+    document.getElementById("germany").onclick = function () {
+        openWindow("germany", "Bundesliga");
+    }
+    document.getElementById("spain").onclick = function () {
+        openWindow("spain", "LaLiga");
+    }
+    document.getElementById("portugal").onclick = function () {
+        openWindow("portugal", "Portugalska");
+    }
+    document.getElementById("italy").onclick = function () {
+        openWindow("italy", "Seria_A");
+    }
+    document.getElementById("brazil").onclick = function () {
+        openWindow("brazil", "Brazylijska");
+    }
+    document.getElementById("england").onclick = function () {
+        openWindow("england", "Angielska");
+    }
+    document.getElementById("france").onclick = function () {
+        openWindow("france", "Ligue1");
+    }
+
+    function openWindow(country, leagueName) {
+        document.getElementById('a' + country).href = "selected_league.php?league=" + leagueName;
+
+    }
+    
     //function
-    function start()
-        {
-            
-             id_numbers = new Array();
-             var geturl = getUrlVars()["league"];//return _$GET from ?league=
-             
-             
+    function start() {
+        id_numbers = new Array();
+        var getUrl = getUrlVars()["league"];
+
         $.ajax({
             method: "GET",
-            url: "php/functions/articles_main/GetAllArticlesFromSelectedLeague.php?league="+geturl,
-            
-            success: function (data) 
-            {
-
+            url: "php/functions/articles_main/GetAllArticlesFromSelectedLeague.php?league=" + getUrl,
+            success: function (data) {
                 id_numbers = JSON.parse(data);
-                
-                getarray(id_numbers);
+                getArray(id_numbers);
             }
-                 });
-        
-        }
-         function getoneArticles(article) 
-          {
-             
-        if (iterator >= numberofarticles.length)
-        {
-           $('#' + arrayarticle[article]+"image").html("");
-           $('#' + arrayarticle[article]+"description").html("");
-           
-            iterator = iterator + 1;
-              
-        } else 
-               {
-                 $('#' + arrayarticle[article]+"image").html("");
-                 $('#' + arrayarticle[article]+"description").html("");
-          
-            
-                  $('#' + arrayarticle[article]+"image").html('<img  src=image/'+numberofarticles[iterator].NAME+'.png width="60" height="60"\n\
-            alt="avatar" style ="border : 2px solid black" />');  
-                 $('#' + arrayarticle[article]+"description").html("<h1>"+numberofarticles[iterator].TITLE+"</h1>"+numberofarticles[iterator].CONTENT);
-             
-                iterator = iterator + 1;
+        });
 
-               }   
-          }
-    function getarray(number) 
-         {
-        numberofarticles = number;
-        
-       ChangeContentOfArticle();
-        
-         }
-    function ChangeContentOfArticle()
+    }
+    
+    function getOneArticle(article)
     {
+        if (iterator >= numOfArticles.length) {
+            $('#' + arrayArticle[article] + "image").html("");
+            $('#' + arrayArticle[article] + "description").html("");
 
+            iterator = iterator + 1;
 
-        for (var i = 0; i <=2; i++)
-        {
-            getoneArticles(i);
+        } else {
+            $('#' + arrayArticle[article] + "image").html("");
+            $('#' + arrayArticle[article] + "description").html("");
 
+            $('#' + arrayArticle[article] + "image").html('<img  src=image/' + numOfArticles[iterator].NAME + '.png width="60" height="60"\n\
+            alt="avatar" style ="border : 2px solid black" />');
+            $('#' + arrayArticle[article] + "description").html("<h1>" + numOfArticles[iterator].TITLE + "</h1>" + numOfArticles[iterator].CONTENT);
+
+            iterator = iterator + 1;
         }
     }
-    function gettable(){
-        var geturl = getUrlVars()["league"];//return _$GET from ?league=
+    
+    function getArray(number) {
+        numOfArticles = number;
+        ChangeContentOfArticle();
+    }
+    
+    function ChangeContentOfArticle() {
+        for (var i = 0; i <= 2; i++) {
+            getOneArticle(i);
+        }
+    }
+    
+    function getTable() {
+        var getUrl = getUrlVars()["league"];
         $.ajax({
             method: "GET",
-            url: "php/functions/table_league.php?league="+geturl,
-            
-            success: function (data) 
-            {
-                
-                 $('#teamTableContent').html(data);
-                
-                
-               
+            url: "php/functions/table_league.php?league=" + getUrl,
+            success: function (data) {
+                $('#teamTableContent').html(data);
             }
-            
-                 });
-        
+        });
     }
-   
-         document.getElementById("next").onclick = function ()
+
+    document.getElementById("next").onclick = function ()
     {
-        if (iterator < numberofarticles.length)
-            ChangeContentOfArticle(numberofarticles);
-
-
-
-
+        if (iterator < numOfArticles.length)
+            ChangeContentOfArticle(numOfArticles);
     };
-    document.getElementById("previous").onclick = function ()
-    {
-
-        if (iterator >= 5)
-        {
-            iterator = iterator - 6;
-            ChangeContentOfArticle(numberofarticles);
-        }
-
-    };
-       function getUrlVars()
-{
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value)
-         {
-            vars[key] = value;
-         });
-    return vars;
-}
     
-        
+    document.getElementById("previous").onclick = function () {
+        if (iterator >= 5)  {
+            iterator = iterator - 6;
+            ChangeContentOfArticle(numOfArticles);
+        }
+    };
+    
+    
+    function getUrlVars() {
+        var vars = {};
+        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+            vars[key] = value;
+        });
+        return vars;
+    }
 });
 
 
